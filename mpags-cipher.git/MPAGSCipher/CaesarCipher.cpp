aa223e081c1246af84e80d2c5ac603f3c540956d
David Bradley <d.bradley.1@warwick.ac.uk>
2019-11-07 14:54:45 +0000

    Added second constructor, made constructors explicit, still referencing errors

--- mpags-cipher.git/MPAGSCipher/CaesarCipher.cpp
+++ mpags-cipher.git/MPAGSCipher/CaesarCipher.cpp
@@ -5,3 +5,21 @@
 CaesarCipher::CaesarCipher(const size_t key) : key_{key}
 {
 }
+
+// Create second constructor that takes a key as string
+
+CaesarCipher::CaesarCipher(const std::string& key) : key_{0}
+{
+   bool isNumber{true};
+   for (const auto& elem : key){
+      if (! std::isdigit(elem)){
+      isNumber = false;
+    }
+    if (isNumber = true){
+      key_ = std::stoul(key);
+    }
+    else{
+      std::cerr << "Input for key must be a number" << std::endl;
+    }
+   }
+}
--- mpags-cipher.git/MPAGSCipher/CaesarCipher.hpp
+++ mpags-cipher.git/MPAGSCipher/CaesarCipher.hpp
@@ -2,10 +2,13 @@
 #define MPAGSCIPHER_CAESARCIPHER_HPP
 
 #include <vector>
+#include <string>
 
 class CaesarCipher {
 public:
-  CaesarCipher(const size_t& key);//Transform to size_t for consistency
+  explicit CaesarCipher(const size_t key);//Transform to size_t for consistency
+  
+  explicit CaesarCipher(const std::string& key);
 
   size_t key_;
 };
